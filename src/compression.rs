9aa955388f5453161a0842343ec837bc9cbcbc08
theaddon <theaddonn@gmail.com>
2025-01-29 13:13:14 +0100

    update compression apis

--- src/compression.rs
+++ src/compression.rs
@@ -1,39 +1,15 @@
-pub struct Compression {
-    level: u32,
-    threshold: u32,
-    method: CompressionMethod,
-}
-
-impl Compression {
-    pub fn new() -> Compression {
-        Compression {
-            level: 0,
-            threshold: 0,
-            method: CompressionMethod::None,
-        }
-    }
-    
-    pub fn level(mut self, level: u32) -> Self {
-        self.level = level;
-        self
-    }
-    
-    pub fn threshold(mut self, threshold: u32) -> Self {
-        self.threshold = threshold;
-        self
-    }
-    
-    pub fn method(mut self, method: CompressionMethod) -> Self {
-        self.method = method;
-        self
-    }
-}
-
-pub enum CompressionMethod {
+pub enum Compression {
     None,
-    Zlib,
-    Snappy,
-    Zstd,
+    Zlib {
+        level: u32,
+        threshold: u32,},
+    Snappy {
+        level: u32,
+        threshold: u32,},
+    Zstd {
+        level: u32,
+        threshold: u32,},
 }
+
 
 
